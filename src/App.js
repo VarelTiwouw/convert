9eccb14228a282928a3a1851d863a5c3d593844c
arturrr03 <rawungito@gmail.com>
2024-11-07 09:54:52 +0800

    Add all components

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import Header  from "./components/Header";
+import Clients from "./components/Clients";
+import Features from "./components/Features";
+import Pricing from "./components/Pricing";
+import Footer from "./components/Footer";
+const App = ()=> {
+  return (
+    <div className="body-wrap">
+      <Header/>
+      <Clients/>
+      <Features/>
+      <Pricing/>
+      <Footer/>
+
+  <main className="site-content">
+    
+  </main>
+  </div>
+  );   
+};
+
+export default App;
